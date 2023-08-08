b336982bd77dfef37fe58644f6989e67337f6224
Devank Garg <79208902+Devank-Garg@users.noreply.github.com>
2023-08-08 22:07:52 +0530

    Updated Image Magician 2.0.py

--- Image Magician 2.0.py
+++ Image Magician 2.0.py
@@ -16,6 +16,9 @@ noise_correction_value = 0
 adjusted_image = None  
 original_image = None  # Image path, working on adding upload button
 saturation_factor=2.5
+crop_x, crop_y, crop_width, crop_height = 0, 0, 400, 300  # Initial crop parameters
+rotate_angle = 0
+
 
 def upload_image():
     global original_image
@@ -61,95 +64,51 @@ def update_gamma(val):
 def apply_gamma_correction(image, gamma):
     gamma_corrected = np.power(image / 255.0, gamma) * 255.0
     return np.uint8(gamma_corrected)
-
-def update_saturation_factor(val):
-    global saturation_factor
-    saturation_factor = val / 10.0
-    add_color_saturation()
-    
-def pick_color_and_get_range(image_path):
-    def mouse_callback(event, x, y, flags, param):
-        nonlocal colorsRGB, stop_display
-        if event == cv2.EVENT_LBUTTONDOWN:
-            colorsBGR = image[y, x]
-            colorsRGB = tuple(reversed(colorsBGR))
-            stop_display = True
-
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (400, 400))
-
-    cv2.namedWindow('PickColorAndGetRange')
-    cv2.setMouseCallback('PickColorAndGetRange', mouse_callback)
-
-    colorsRGB = None
-    stop_display = False
-
-    while not stop_display:
-        cv2.imshow('PickColorAndGetRange', image)
-        if cv2.waitKey(10) & 0xFF == 27:
-            break
-
-    cv2.destroyWindow('PickColorAndGetRange')
-    if colorsRGB is not None:
-        picked_color_hsv = cv2.cvtColor(np.uint8([[colorsRGB]]), cv2.COLOR_BGR2HSV)
-        hue_range = 15
-        color_range = (picked_color_hsv[0][0][0] - hue_range, 40, 40), (picked_color_hsv[0][0][0] + hue_range, 255, 255)
-        return color_range
-    else:
-        return None
-
-def change_color_saturation(image, color_range, saturation_factor):
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    lower_bound = np.array(color_range[0], dtype=np.uint8)
-    upper_bound = np.array(color_range[1], dtype=np.uint8)
-
-    mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
-
-    h, s, v = cv2.split(hsv_image)
-
-    s[mask > 0] = np.clip(s[mask > 0] * saturation_factor, 0, 255)
-    hsv_image = cv2.merge([h, s, v])
-
-    output_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-
-    return output_image
+def update_crop(x, y, width, height):
+    global crop_x, crop_y, crop_width, crop_height
+    crop_x, crop_y, crop_width, crop_height = x, y, width, height
+    update_image()
 
 
-def choose_color():
-    global color_range, saturation_factor
-    color_code = colorchooser.askcolor(title="Choose color")
-    if color_code[1] is not None:
-        picked_color_hsv = cv2.cvtColor(np.uint8([[color_code[0]]]), cv2.COLOR_BGR2HSV)
-        hue_range = 15  
-        color_range = (picked_color_hsv[0][0][0] - hue_range, 40, 40), (picked_color_hsv[0][0][0] + hue_range, 255, 255)
-        add_color_saturation()
-        
-def choose_color2():
-    global color_range
 
-    color_range = pick_color_and_get_range(file_path) 
-    add_color_saturation()
-    
+def crop_image():
+    global original_image
+    global adjusted_image
+    if original_image is not None:
+        cropped_image = original_image[crop_y:crop_y + crop_height, crop_x:crop_x + crop_width]
+        adjusted_image = cropped_image.copy()
+        cv2.imshow('Image Adjustments', adjusted_image)
+
+def update_rotate(angle):
+    global rotate_angle
+    rotate_angle = angle
+    update_image()
+
+def rotate_image():
+    global adjusted_image
+    if original_image is not None:
+        rows, cols, _ = original_image.shape
+        rotation_matrix = cv2.getRotationMatrix2D((cols / 2, rows / 2), rotate_angle, 1)
+        adjusted_image = cv2.warpAffine(original_image, rotation_matrix, (cols, rows))
+        cv2.imshow('Image Adjustments', adjusted_image)
 
     
 def update_image():
-    global adjusted_image  
+    global adjusted_image
     if original_image is not None:
         hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
         hsv_image[..., 2] = cv2.convertScaleAbs(hsv_image[..., 2], alpha=brightness_value / 50.0)
         hsv_image[..., 2] = cv2.convertScaleAbs(hsv_image[..., 2], alpha=contrast_value / 50.0)
         hsv_image[..., 1] = cv2.convertScaleAbs(hsv_image[..., 1], alpha=saturation_value / 50.0)
         hsv_image[..., 0] = (hsv_image[..., 0] + hue_shift_value) % 180
-        
+
         noise_mask = np.random.normal(0, noise_correction_value, hsv_image.shape[:2])
         hsv_image[..., 2] += noise_mask.astype(np.uint8)
-        
-        
+
         adjusted_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-        
-        # Apply gamma correction
         adjusted_image = apply_gamma_correction(adjusted_image, gamma_value)
+
+        cv2.imshow('Image Adjustments', adjusted_image)
         
         
         
@@ -170,30 +129,9 @@ def add_blur():
         blurred_image=cv2.GaussianBlur(adjusted_image,(5,5),0)
         cv2.imshow('Image Adjustments',blurred_image)
         
-def add_color_saturation():
-        global original_image
-        global adjusted_image
-        global color_range, saturation_factor
-        if adjusted_image is None:
-            color_image=change_color_saturation(original_image, color_range, saturation_factor)
-            color_image=cv2.resize(color_image, (400, 300))
-            cv2.imshow("Image Adjustments",color_image)
-        else:
-            
-            modified_image = change_color_saturation(adjusted_image, color_range, saturation_factor)
-            modified_image_resized = cv2.resize(modified_image, (400, 400))
-            cv2.imshow("Image Adjustments",modified_image_resized)
-            
 
         
         
-    
-
-    
-
-    
-  
-
 # Creating separate cv2 window and the trackbar to change different image options
 cv2.namedWindow('Image Adjustments')
 cv2.createTrackbar('Brightness', 'Image Adjustments', brightness_value, 100, update_brightness) # change brightness
@@ -202,7 +140,13 @@ cv2.createTrackbar('Hue Shift', 'Image Adjustments', hue_shift_value, 180, updat
 cv2.createTrackbar('Noise Correction', 'Image Adjustments', noise_correction_value, 50, update_noise_correction) # Change noise
 cv2.createTrackbar('Contrast', 'Image Adjustments', contrast_value, 100, update_contrast)   # change contrast
 cv2.createTrackbar('Gamma', 'Image Adjustments', 10, 30, update_gamma) 
-cv2.createTrackbar('Saturation Factor', 'Image Adjustments', int(saturation_factor*10) , 50, update_saturation_factor)
+cv2.createTrackbar('Crop X', 'Image Adjustments', 0, 400, lambda x: update_crop(x, crop_y, crop_width, crop_height))
+cv2.createTrackbar('Crop Y', 'Image Adjustments', 0, 300, lambda y: update_crop(crop_x, y, crop_width, crop_height))
+cv2.createTrackbar('Crop Width', 'Image Adjustments', 400, 400, lambda w: update_crop(crop_x, crop_y, w, crop_height))
+cv2.createTrackbar('Crop Height', 'Image Adjustments', 300, 300, lambda h: update_crop(crop_x, crop_y, crop_width, h))
+cv2.createTrackbar('Rotate', 'Image Adjustments', 0, 360, update_rotate)
+
+
 
 
 def save_image():
@@ -247,11 +191,13 @@ save_button = tk.Button(root, text="Save Image",font=("Helvetica", 12,'bold'), b
 save_button.pack(pady=35)
 Blur_button=tk.Button(root,text='Add Gaussian Blur ',font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5, command=add_blur)
 Blur_button.pack(pady=35)
+crop_button = tk.Button(root, text="Crop Image", font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5, command=crop_image)
+crop_button.pack( pady=45)
 
-color_button=tk.Button(root,text='Pick Colour ',font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5, command=choose_color)
-color_button.pack(pady=40)
-color_button2=tk.Button(root,text='Pick Colour from Image',font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5, command=choose_color2)
-color_button2.pack(pady=45)
+rotate_button = tk.Button(root, text="Rotate Image", font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5, command=rotate_image)
+rotate_button.pack(pady=50)
+
+
 
 exit_button=tk.Button(root,text='Quit ?',font=("Helvetica", 12,'bold'), bg='grey',borderwidth=5,command=exit)
 exit_button.pack(side='bottom')
